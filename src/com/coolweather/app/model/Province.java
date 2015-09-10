c70106a3d7fc05b65e1d1fdefce81c8e6f4f5218
XiaoLiu <xiaoliucool@126.com>
2015-09-10 17:24:57 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ src/com/coolweather/app/model/Province.java
@@ -0,0 +1,30 @@
+package com.coolweather.app.model;
+/**
+ * province表对应的实体
+ * @author Administrator
+ *
+ */
+public class Province {
+	private int id;
+	private String provinceName;
+	private String provinceCode;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getProvinceName() {
+		return provinceName;
+	}
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+	public String getProvinceCode() {
+		return provinceCode;
+	}
+	public void setProvinceCode(String provinceCode) {
+		this.provinceCode = provinceCode;
+	}
+	
+}
